a9964cab490b80c406c45c86874d39b185b8c210
bechirMakhlouf <bechirM@proton.me>
2023-12-01 22:30:19 +0100

    fix hero section heading size

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -18,13 +18,13 @@ const HeroSection = (
     <>
       <div className="w-full flex justify-center">
         <div className="w-full lg:w-[90%] xl:w-[80%] md:px-6 pb-6">
-          <div className="w-full gap-y-12 gap-x-4 xl:min-h-screen bg-background flex xl:flex-nowrap flex-wrap justify-stretch items-stretch">
+          <div className="w-full gap-y-12 gap-x-4 xl:min-h-screen bg-background flex flex-col lg:flex-row lg:justify-stretch lg:items-stretch">
             
             <div className="w-full min-h-[80vh] md:min-h-[80vh] pt-12 xl:w-[50%] h-full flex justify-center items-center md:items-center ">
 
               <div className="w-full px-6 flex flex-col justify-center items-center xl:pt-16 xl:justify-start gap-y-12">
 
-                <h1 className="text-4xl sm:text-5xl md:text-7xl text-center break-words">
+                <h1 className="text-[3vw] text-center break-words">
                   {heroSectionContent.message}
                 </h1>
                 <p className="px-2 md:indent-11 text-md text-center xl:text-start">
@@ -38,7 +38,7 @@ const HeroSection = (
               </div>
             </div>
 
-            <div className="self-center px-4 xl:p-0 w-full h-full xl:w-[50%] flex justify-center items-center">
+            <div className="self-center px-4 xl:p-0 w-1/2 h-full flex justify-center items-center">
               <ImageVariant1 img={heroSectionContent.image} />
             </div>
           </div>
